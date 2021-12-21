b2b36368ee4dfe79353fb4a0434de7a126b610e4
DIGIKRASH <krashmate@gmail.com>
2021-12-21 21:28:36 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Jquery ----------------
+@@include('../../node_modules/jquery/dist/jquery.min.js');
+
+
+// Plugins ----------------
+
+
+
+// Main-JS -----------------
+@@include('../js/main.js');
+
+
